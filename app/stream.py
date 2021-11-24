64cd8209a5c228d7521a9ed55a47299e9aff5d39
Nadia Yap <odysseynadia@gmail.com>
2021-11-24 19:00:47 +1100

    Worked on a mock juxtapose visualisation

--- app/stream.py
+++ app/stream.py
@@ -7,6 +7,16 @@ import tempfile
 from idc.processing import split
 import requests
 
+# Juxtapose import
+from streamlit_juxtapose import juxtapose
+import pathlib
+
+STREAMLIT_STATIC_PATH = (
+    pathlib.Path(st.__path__[0]) / "static"
+)  # at venv/lib/python3.9/site-packages/streamlit/static
+
+IMG1 = "img1.png"
+IMG2 = "img2.png"
 
 st.write("""bro""")
 
@@ -16,10 +26,14 @@ png = st.file_uploader("Upload a PNG image", type=([".png"]))
 
 if png:
 
-    st.image(png)  # display image
+    # st.image(png)  # display image
 
     # preprocessing
     image = Image.open(png)
+    image.save(STREAMLIT_STATIC_PATH / IMG1)
+    juxtapose(IMG1,IMG1)
+
+
     array_of_images = split(image)
 
     bytes_image = base64.b64encode(array_of_images)
