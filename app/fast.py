e00d2668bd1f697be438b251a3a08b73e2e5f97b
Jack Claar <jack.claar@gmail.com>
2021-11-24 14:23:25 +0900

    shuffling things around

--- app/fast.py
+++ app/fast.py
@@ -8,6 +8,7 @@ from io import BytesIO
 from typing import Optional
 import base64
 from idc.processing import split
+from idc.gradcam import make_gradcam_heatmap, save_and_display_gradcam
 
 
 app = FastAPI()
@@ -44,10 +45,22 @@ def predict(file: bytes = File(...)):
     return {"prediction": prediction}
 
 
-# @app.get("/predict/aoi")
-# def areas_of_interest():
-#     pass
+@app.post("/annotate")
+def annotate(file: bytes = File(...)):
+
+    file_decode = base64.b64decode(file)
+    long_array = np.frombuffer(file_decode, dtype=np.uint8)
+
+    count = long_array.shape[0] // 7500
+    pics = np.reshape(long_array, (count, 50, 50, 3)) / 255
+
+    model = load_model("model.h5")
+
+    prediction = float(model.predict(pics)[:, 0][0])
+
+    prediction = [float(num) for num in model.predict(pics)[:, 0]]
+    return {"prediction": prediction}
 
 
 if __name__ == "__main__":
-    predict()
+    annotate()
