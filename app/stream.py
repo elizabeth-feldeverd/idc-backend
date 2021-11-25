e3018b45030df423151569a200f245b9f906dacf
Jack Claar <jack.claar@gmail.com>
2021-11-25 13:54:12 +0900

    added slider to heatmap

--- app/stream.py
+++ app/stream.py
@@ -6,18 +6,14 @@ import base64
 import tempfile
 from idc.processing import split
 import requests
+from streamlit_juxtapose import juxtapose
+import pathlib
 
-
-# Juxtapose import
-# from streamlit_juxtapose import juxtapose
-# import pathlib
-
-# STREAMLIT_STATIC_PATH = (
-#     pathlib.Path(st.__path__[0]) / "static"
-# )  # at venv/lib/python3.9/site-packages/streamlit/static
-
-# IMG1 = "img1.png"
-# IMG2 = "img2.png"
+STREAMLIT_STATIC_PATH = (
+    pathlib.Path(st.__path__[0]) / "static"
+)  # at venv/lib/python3.9/site-packages/streamlit/static
+IMG1 = "img1.png"
+IMG2 = "img2.png"
 
 you_want = False
 
@@ -28,17 +24,25 @@ png = st.file_uploader("Upload a PNG image", type=([".png"]))
 
 
 if png:
-
-    st.image(png)  # display image
+    # st.image(png)  # display image
 
     url = "http://127.0.0.1:8000/annotate"
-
     files = {"file": (png.name, png, "multipart/form-data")}
-
-    # put a spinny wheel while waiting for the response
     response = requests.post(url, files=files)
 
-    st.image(response._content)
+    # put a spinny wheel while waiting for the response
+
+    # st.image(response._content)  # display heatmap
+
+    original = Image.open(png)
+    original.save(STREAMLIT_STATIC_PATH / IMG1)
+
+    # if push comes to shove vvv
+    file = open(f"{STREAMLIT_STATIC_PATH}/{IMG2}", "wb")
+    file.write(response.content)
+    file.close()
+
+    juxtapose(IMG1, IMG2)
 
     if you_want:
         url = "http://127.0.0.1:8000/predict"
